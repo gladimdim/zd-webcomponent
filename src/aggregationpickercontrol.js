595bbe214b66f7ac503b466c9984b6a8c958fb4e
Dmytro Gladkyi <dmytro.gladkyi@zoomdata.com>
2017-10-03 15:46:04 +0300

    Initial commit.
    App can be opened in Chrome/Safari

---
+++ src/aggregationpickercontrol.js
@@ -0,0 +1,40 @@
+import {Element as PolymerElement} from '../node_modules/@polymer/polymer/polymer-element.js';
+
+class AggregationPickerControl extends PolymerElement {
+    static get template() {
+        return `
+            <div id="aggregations">
+            </div>
+        `;
+    }
+    constructor() {
+        super();
+    }
+    static get properties() {
+        return {
+            aggregations: {
+                observer: "aggregationsSet",
+                Type: Array
+            }
+        };
+    }
+    aggregationsSet(aggrs) {
+        const aggrNames = aggrs.map((aggr) => aggr.getName());
+        const rootElement = document.createElement("select");
+        rootElement.setAttribute("name", "aggregations");
+        for (let name of aggrNames) {
+            const optionElement = document.createElement("option");
+            optionElement.textContent = name;
+            rootElement.appendChild(
+                optionElement
+            );
+        }
+        this.$.aggregations.innerHTML = '';
+        this.$.aggregations.appendChild(rootElement);
+        rootElement.addEventListener("change", (newValue) => {
+            this.dispatchEvent(new CustomEvent("selected", {detail: newValue.target.value}));
+        });
+    }
+}
+
+customElements.define("aggregation-picker-control", AggregationPickerControl);
---
+++ src/zoomdata-visualization.js
@@ -0,0 +1,169 @@
+import {Element as PolymerElement} from '../node_modules/@polymer/polymer/polymer-element.js';
+import * as sdk from '../node_modules/zoomdata-client/distribute/sdk/zoomdata-client.js';
+import * as AggregationControl from './aggregationpickercontrol.js';
+
+export class MyApp extends PolymerElement {
+    static get template() {
+        return `
+        <style>
+            .wrapper {
+                display: grid;
+                grid-template-columns: 2rem auto 2rem;
+                grid-template-rows: 2rem auto 2rem;
+                width: 100%;
+                height: 100%;
+            }
+            .vis-content {
+                grid-column-start: 2;
+                grid-column-end: 2;
+                grid-row-start: 2;
+                grid-row-end: 2;
+            }
+            .vis-content-top {
+                grid-column-start: 1;
+                grid-column-end: 4;
+                grid-row-start: 1;
+                grid-row-end: 1;
+                width: 100%;
+                height: 100%;
+            }
+            .vis-content-left {
+                grid-column-start: 1;
+                grid-column-end: 1;
+                grid-row-start: 1;
+                grid-row-end: 4;
+                transform-origin: 0 0;
+                transform:  translateY(100%) rotate(-90deg);
+
+            }
+            .vis-content-right {
+                grid-column-start: 3;
+                grid-column-end: 4;
+                grid-row-start: 1;
+                grid-row-end: 4;
+                transform-origin: 0 0;
+                transform: rotate(90deg);
+
+            }
+            .vis-content-bottom {
+                grid-column-start: 1;
+                grid-column-end: 4;
+                grid-row-start: 3;
+                grid-row-end: 3;
+            }
+        </style>
+        <div class="wrapper">
+            <aggregation-picker-control id="controls" class="vis-content-top"></aggregation-picker-control>
+            <div id="visContainer" class="vis-content" style="width: 100%; height: 100%"/>
+        </div>
+        `;
+    }
+    connectedCallback() {
+        super.connectedCallback();
+        initApp(this);
+    }
+    static get properties () {
+        return {
+            path: {
+                type: String,
+                reflectToAttribute: true
+            },
+            host: {
+                type: String,
+                reflectToAttribute: true
+            },
+            port: {
+                type: Number,
+                reflectToAttribute: true
+            },
+            key: {
+                type: String,
+                reflectToAttribute: true
+            },
+            secure: {
+                type: Boolean,
+                reflectToAttribute: true
+            },
+            sourceName: {
+                type: String,
+                reflectToAttribute: true
+            },
+            visualization: {
+                type: String,
+                reflectToAttribute: true
+            },
+            layout: {
+                type: String,
+                value: 'control-top'
+            }
+        };
+    }
+}
+async function initializeClient(app) {
+    const client = await ZoomdataSDK.createClient({
+        credentials: {
+            key: app.key
+        },
+        application: {
+                secure: app.secure,
+                host: app.host,
+                port: app.port,
+                path: app.path
+        }
+    });
+    return client;
+}
+
+const visualize = async (doc) => {
+    const client = await initializeClient(doc);
+    const query = await client.createQuery(
+        {name: doc.sourceName},
+        {
+            groups: [
+                {
+                    'name': 'gender',
+                    'limit': 50,
+                    'sort': {
+                        'dir': 'asc',
+                        'name': 'gender'
+                    }
+                }
+            ],
+            metrics: [
+                {
+                    name: "satisfaction",
+                    func: "sum"
+                }
+            ]
+        }
+    );
+    const visualization = await client.visualize({
+        element: doc.$.visContainer,
+        query: query,
+        visualization: doc.visualization,
+        variables: {}
+    });
+    return visualization;
+};
+
+
+const initApp = async (doc) => {
+    const visualization = await visualize(doc);
+    visualization.query.validationErrors.subscribeOnNext((err) => {
+        console.log(err);
+    });
+    const rootDom = doc.$.controls;
+    const metaData = visualization.metaThread.getLatestResponse();
+    const aggrs = metaData.getAttrAggregations().filter((aggr) => {
+        return aggr.getType() === "TERMS";
+    });
+
+    doc.$.controls.aggregations = aggrs;
+    doc.$.controls.addEventListener("selected", (e) => {
+        const firstAggregation = visualization.query.getAggregations(0);
+        firstAggregation[0].field.name = e.detail;
+        visualization.query.setAggregation(0, 0, firstAggregation[0]);
+    });
+}
+
+customElements.define("zoomdata-visualization", MyApp);
